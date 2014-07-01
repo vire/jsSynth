cc6eae74531881ee7e956c6e2126c939e2cbdf55
vire <viliam.elischer@gmail.com>
2014-07-02 00:01:04 +0200

    simple gulp support for sequencer

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'),
+		concat = require('gulp-concat'),
+		uglify = require('gulp-uglify'),
+		del = require('del');
+    
+var paths = {
+	scripts: [
+		'js/src/eventmanager.js',
+		'js/src/looper.js',
+		'js/src/tempomat.js',
+		'js/src/uimanager.js',
+		'js/src/sequencer.js',
+		'js/src/sequencerloader.js',
+		'js/src/debugpanel.js'
+		]
+}
+
+gulp.task('clean', function(cb) {
+  del(['dist'], cb);
+})
+
+gulp.task('scripts', ['clean'], function() {
+	return gulp.src(paths.scripts)
+		.pipe(uglify())
+		.pipe(concat('seq.min.js'))
+		.pipe(gulp.dest('dist'));
+
+})
+gulp.task('default', ['scripts']);
